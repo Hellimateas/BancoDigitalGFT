d7766d1c6629bec45ac949ab42973b1afdacf58b
Hellimateas Barros <hellimateasbarros@gmail.com>
2022-01-26 20:55:48 -0300

    Criação do pacote conta e as integraçoes

---
+++ src/com/dio/conta/Conta.java
@@ -0,0 +1,54 @@
+package com.dio.conta;
+
+import com.dio.Cliente;
+
+public class Conta {
+	private static final int AGENCIA_PADRAO = 1;
+	private static int SEQUENCIAL = 1;
+
+	protected int numero;
+	protected int agencia;
+	protected double saldo;
+	protected Cliente cliente;
+	
+	public Conta(Cliente cliente) {
+		this.agencia = Conta.AGENCIA_PADRAO;
+		this.numero = SEQUENCIAL++;
+		this.cliente = cliente;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void saque(double valor) {
+		this.saldo -= valor;
+	}
+
+	public void deposito(double valor) {
+		this.saldo += valor;
+	}
+
+	public void transferencia(Conta conta, double valor) {
+		this.saque(valor);
+		conta.deposito(valor);
+	}
+	
+	public void infoExtrato() {
+		System.out.println("   Número da conta---- " + this.numero);
+		System.out.println("   Número da agência-- " + this.agencia);
+		System.out.println("   Saldo ------------- " + this.saldo);
+	}
+}
---
+++ src/com/dio/conta/ContaCorrente.java
@@ -0,0 +1,11 @@
+package com.dio.conta;
+
+public class ContaCorrente {
+	
+	public void imprimir() {
+		System.out.println("Conta Corrente");
+		System.out.println(" -> Extrato:");
+		System.out.println();
+	}
+
+}
